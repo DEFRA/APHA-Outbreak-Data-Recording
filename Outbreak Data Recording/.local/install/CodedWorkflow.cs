3ff616b8c25391bf9a748b57470f1c811c4694a8
RobMDefra <132434695+RobMDefra@users.noreply.github.com>
2025-03-28 09:40:17 +0000

    Moved workflows, continued NDOMS nav, Forms dependency issue

--- Outbreak Data Recording/.local/install/CodedWorkflow.cs
+++ Outbreak Data Recording/.local/install/CodedWorkflow.cs
@@ -25,7 +25,7 @@ namespace OutbreakDataRecording
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Word.Activities.API.IWordService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Mail.Activities.Api.IMailService)};
+            _ = new System.Type[]{typeof(UiPath.Mail.Activities.Api.IMailService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Word.Activities.API.IWordService)};
         }
 
         protected UiPath.Mail.Activities.Api.IMailService mail { get => serviceContainer.Resolve<UiPath.Mail.Activities.Api.IMailService>(); }
