10d30ffd67f8f8ab50d1650c709e7df7d6f20cf4
RobMDefra <132434695+RobMDefra@users.noreply.github.com>
2025-04-14 13:23:04 +0100

    check species

--- Outbreak Data Recording/.local/install/CodedWorkflow.cs
+++ Outbreak Data Recording/.local/install/CodedWorkflow.cs
@@ -25,7 +25,7 @@ namespace OutbreakDataRecording
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Word.Activities.API.IWordService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Mail.Activities.Api.IMailService), typeof(UiPath.Core.Activities.API.ISystemService)};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Mail.Activities.Api.IMailService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Word.Activities.API.IWordService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
 
         protected UiPath.Mail.Activities.Api.IMailService mail { get => serviceContainer.Resolve<UiPath.Mail.Activities.Api.IMailService>(); }
